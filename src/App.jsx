f818d7b436267516a059df580a01dc56045ae7f8
Himanshu-Dubey-1 <Himanshudubey241@gmail.com>
2024-06-05 00:55:29 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [text, settext] = useState('')
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const [hoh, sethoh] = useState('')
+
+  const handletextChange = (e) => {
+    settext(e.target.value);
+};
+  const handleemailChange = (e) => {
+    e.preventDefault();
+    setemail(e.target.value);
+};
+  const handlepasswordChange = (e) => {
+    e.preventDefault();
+    setpassword(e.target.value);
+};
+  
+const ohh= () => {
+  sethoh('Please enter valid credentials');
+};
+
+
+   const check = () => {
+    ohh()
+    e.preventDefault();
+//     if (text==='') { 
+//       ohh()
+//     } else {
+//       sethoh('')
+//     }
+   };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='inputField'>
+
+      <input type="text" placeholder='name' value={text} onChange={handletextChange} />
+      <span>{!text?hoh:''}</span>
+
+      <input type="text" placeholder='email' value={email} onChange={handleemailChange} />
+      <span>{!email?hoh:''}</span>
+
+      <input type="text" placeholder='password' value={password} onChange={handlepasswordChange}/>
+      <span>{!password?hoh:''}</span>
+
+      <button onClick={check}>Submit</button>
+    </div>
     </>
   )
 }
